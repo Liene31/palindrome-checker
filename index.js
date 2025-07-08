f518af79b258d39ef501d70aa9614b47480aac25
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-07-08 12:09:54 +0200

    Add palindrome checker functionality to UI
    Introduced a palindrome checking feature with input and button elements in the HTML, implemented the palindrome logic and result display in JavaScript, and added styles for the answer field and highlighted text in CSS.

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 //button is non-active (different color, without pointer)
 //Only when text added, it becomes active(color + pointer)
+
+const checkBtn = document.getElementById("check-btn");
+const inputValue = document.getElementById("input-value");
+const answerField = document.getElementById("answer-field");
+
+function isPalindrome(word) {
+  const phrase = word.replace(/\s+/g, "").toLowerCase();
+  let reversedPhrase = "";
+  for (let i = phrase.length - 1; i >= 0; i--) {
+    reversedPhrase += phrase[i];
+  }
+
+  return phrase === reversedPhrase;
+}
+
+checkBtn.addEventListener("click", () => {
+  const para = document.createElement("p");
+  const span = document.createElement("span");
+  const phrase = inputValue.value;
+
+  span.className = "text-highlight";
+  span.textContent = `'${phrase}'`;
+  para.textContent = "Yes, ";
+  para.appendChild(span);
+  para.append(" is a palindrome!");
+  document.getElementById("answer-field").append(para);
+  console.log(isPalindrome(phrase));
+});
